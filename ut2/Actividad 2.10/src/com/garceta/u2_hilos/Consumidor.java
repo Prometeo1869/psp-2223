4d3e33a1254f4f3b32ec60c6f39afeab0933a5e8
Prometeo1869 <juancebrianp@gmail.com>
2022-12-11 21:53:31 +0100

    GARCETA: 2.10 Hilos, Productor-Consumidor

---
+++ ut2/Actividad 2.10/src/com/garceta/u2_hilos/Consumidor.java
@@ -0,0 +1,26 @@
+package com.garceta.u2_hilos;
+
+public class Consumidor extends Thread {
+    private Cola cola;
+    private String s;
+
+    public Consumidor(Cola c) {
+        cola = c;
+    }
+
+    @Override
+    public void run() {
+        String cadena = "";
+        synchronized (cola) {
+            while(true) {
+                s = cola.get(); //recoge la cadena de la cola
+                System.out.print(s + " ");
+                try {
+                    sleep(500);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }//synchronized
+    }//run
+}//Consumidor
